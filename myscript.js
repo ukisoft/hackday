dfa3c20ec98343b1c23af70602dfe02b09b3e8ca
ukisoft <msorakubo@msn.com>
2013-07-16 13:25:25 +0900

    bar blocks appear

--- myscript.js
+++ myscript.js
@@ -20,25 +20,29 @@ function draw() {
 		showIcon(stageWidth, stageHeight, iconSideSpace, iconWidth, iconIconSpace, i);
 	}
 	
-	
+	//バーを表示...test
+	for (i = 0; i < 10; i++) {
+		showBarBlock(i);
+	}
 }
 
 function prepareCanvas() {
 	//htmlの要素を確認。なければエラー。
 	var canvas = document.getElementById('mycanvas');
 	if (!canvas || !canvas.getContext) return false;
-	return canvas;
+	var rtnCanvas = canvas.getContext('2d');
+	return rtnCanvas;
 }
 
 function showStageArea(stageWidth, stageHeight) {
-	var canvas = prepareCanvas();
+	//var canvas = prepareCanvas();
 	
 	//外枠（削除予定）
-	var stageArea = canvas.getContext('2d');
+	var stageArea = prepareCanvas();//canvas.getContext('2d');
 	stageArea.strokeRect(0, 0, stageWidth, stageHeight);
 	
 	//baseline
-	var baseline = canvas.getContext('2d');
+	var baseline = prepareCanvas();//canvas.getContext('2d');
 	baseline.beginPath();
 	baseline.moveTo(0, stageHeight * (8/10));
 	baseline.lineTo(stageWidth, stageHeight * (8/10));
@@ -46,22 +50,17 @@ function showStageArea(stageWidth, stageHeight) {
 }
 
 function showIcon(stageWidth, stageHeight, iconSideSpace, iconWidth, iconIconSpace, teamNumber) {
-	var iconImg = new Image();
-	//var iconFilePath = new iconImg.File("icon" + teamNumber + ".jpg");
-	//if (iconFilePath.exists) {
-	
-		
+	var iconImg = new Image();		
 		iconImg.src = "icon" + teamNumber + ".jpg";
-		//if (iconImg.FileExists) {
-			var canvas = prepareCanvas();
-			var icon = canvas.getContext('2d');
-			
-			var positionY = stageHeight * (8/10) + 10;
-			var positionX = iconSideSpace + (iconWidth + iconIconSpace) * teamNumber;
-			var iconHeight = iconImg.height * (iconWidth / iconImg.width);
-					
-			iconImg.onload = function() {
-				icon.drawImage(iconImg, positionX, positionY, iconWidth, iconHeight);
+		//var canvas = prepareCanvas();
+		var icon = prepareCanvas();//canvas.getContext('2d');
+		
+		var positionY = stageHeight * (8/10) + 10;
+		var positionX = iconSideSpace + (iconWidth + iconIconSpace) * teamNumber;
+		var iconHeight = iconImg.height * (iconWidth / iconImg.width);
+				
+		iconImg.onload = function() {
+			icon.drawImage(iconImg, positionX, positionY, iconWidth, iconHeight);
 		}
 	//}
 	/*else {
@@ -73,4 +72,7 @@ function showIcon(stageWidth, stageHeight, iconSideSpace, iconWidth, iconIconSpa
 	}*/
 }
 
-
+function showBarBlock(blockRow) {
+	var block = prepareCanvas();
+	block.strokeRect(10 * blockRow, 10 * blockRow, 8, 8);
+}
