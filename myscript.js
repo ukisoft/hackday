8da960daf4d584b799d4b02f2d3eec1ddb26b41e
ukisoft <msorakubo@msn.com>
2013-07-16 10:18:12 +0900

    icons show
    but, sometimes they can not appear.

--- myscript.js
+++ myscript.js
@@ -4,23 +4,39 @@ window.onload = function() {
 }
 
 function draw() {
+	var stageWidth = 800;
+	var stageHeight = 600;
+	var iconSideSpace = 20;
+	var iconWidth = 50;
+	var teamMaxNumber = 8;
+	
+	var iconIconSpace = (stageWidth - (2 * iconSideSpace) - (teamMaxNumber * iconWidth)) / (teamMaxNumber - 1);
+	
+	//var canvas = prepareCanvas();
+	
+	//stageを描写
+	showStageArea(stageWidth, stageHeight);
+	
+	//icon表示
+	for (i = 0; i < teamMaxNumber; i++) {
+		showIcon(stageWidth, stageHeight, iconSideSpace, iconWidth, iconIconSpace, i);
+	}
+	
+	//描写の種類（2dしかない）
+	//var ctx = canvas.getContext('2d');
+	
+	//ctx.strokeRect(10, 10, 50, 50);
+}
+
+function prepareCanvas() {
 	//htmlの要素を確認。なければエラー。
 	var canvas = document.getElementById('mycanvas');
 	if (!canvas || !canvas.getContext) return false;
-	
-	//stageを描写
-	showStageArea(800, 600);
-	
-	//描写の種類（2dしかない）
-	var ctx = canvas.getContext('2d');
-	
-	ctx.strokeRect(10, 10, 50, 50);
+	return canvas;
 }
 
 function showStageArea(stageWidth, stageHeight) {
-	//htmlの要素を確認。なければエラー。
-	var canvas = document.getElementById('mycanvas');
-	if (!canvas || !canvas.getContext) return false;
+	var canvas = prepareCanvas();
 	
 	//外枠（削除予定）
 	var stageArea = canvas.getContext('2d');
@@ -32,4 +48,28 @@ function showStageArea(stageWidth, stageHeight) {
 	baseline.moveTo(0, stageHeight * (8/10));
 	baseline.lineTo(stageWidth, stageHeight * (8/10));
 	baseline.stroke();
+}
+
+function showIcon(stageWidth, stageHeight, iconSideSpace, iconWidth, iconIconSpace, teamNumber) {
+	var iconImg = new Image();
+	iconImg.src = "icon" + teamNumber + ".jpg";
+	//if (iconImg.FileExists) {
+		var canvas = prepareCanvas();
+		var icon = canvas.getContext('2d');
+		
+		var positionY = stageHeight * (8/10) + 10;
+		var positionX = iconSideSpace + (iconWidth + iconIconSpace) * teamNumber;
+		var iconHeight = iconImg.height * (iconWidth / iconImg.width);
+				
+		iconImg.onload = function() {
+			icon.drawImage(iconImg, positionX, positionY, iconWidth, iconHeight);
+		}
+	/*}
+	else {
+		var canvas = prepareCanvas();
+		var context = canvas.getContext('2d');
+		context.font = "20pt Arial";
+		var teamNumberPlus = teamNumber + 1;
+		context.fillText(teamNumberPlus + "班", positionX, positionY);
+	}*/
 }
